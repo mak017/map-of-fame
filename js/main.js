64b6d969997bd21084583e8e336f69de9ea3c0e2
mak017 <makovskyy0983@gmail.com>
2020-09-08 21:58:06 +0300

    Added dark layer to map

--- js/main.js
+++ js/main.js
@@ -1,26 +1,40 @@
-// async function getData(url) {
-
-// }
+const state = {
+  mode: 'light',
+};
 
 document.addEventListener('DOMContentLoaded', function () {
-  // Init leaflet map
-  const map = L.map('map');
-  const addSpotSidebar = document.querySelector('.add-spot');
-
-  // Tile layer
-  L.tileLayer(
+  // Tile layers
+  const lightLayer = L.tileLayer(
     'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
     {
       attribution:
         'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
-      id: 'mapbox/streets-v11',
+      id: 'mapbox/light-v10',
       tileSize: 512,
       zoomOffset: -1,
       accessToken:
         'pk.eyJ1IjoibWFrMDE3IiwiYSI6ImNrZXB1ZjlicTFoNmsydm84a3YybTczczkifQ.Tk6EsX0mlVOljcEYD-HXMw',
     },
-  ).addTo(map);
+  );
+
+  const darkLayer = L.tileLayer(
+    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/dark-v10',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken:
+        'pk.eyJ1IjoibWFrMDE3IiwiYSI6ImNrZXB1ZjlicTFoNmsydm84a3YybTczczkifQ.Tk6EsX0mlVOljcEYD-HXMw',
+    },
+  );
+
+  // Init leaflet map
+  const map = L.map('map', { layers: [lightLayer] });
+  const addSpotSidebar = document.querySelector('.add-spot');
 
   // Change position of zoom control
   map.zoomControl.setPosition('bottomleft');
@@ -45,7 +59,7 @@ document.addEventListener('DOMContentLoaded', function () {
     shadowAnchor: [23, 43],
   });
 
-  document.querySelector('.addSpot').addEventListener('click', function () {
+  document.querySelector('.button-add_spot').addEventListener('click', function () {
     const center = map.getCenter();
     const newMarker = L.marker(center, { draggable: true, icon: newMarkerIcon }).addTo(map);
     newMarker.addEventListener('moveend', function () {
@@ -53,5 +67,17 @@ document.addEventListener('DOMContentLoaded', function () {
         addSpotSidebar.classList.add('visible');
       }
     });
+  });
+
+  document.querySelector('.button-dark_mode').addEventListener('click', function () {
+    if (state.mode === 'light') {
+      map.removeLayer(lightLayer);
+      map.addLayer(darkLayer);
+      state.mode = 'dark';
+    } else {
+      map.removeLayer(darkLayer);
+      map.addLayer(lightLayer);
+      state.mode = 'light';
+    }
   });
 });
