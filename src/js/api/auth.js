9b5f1cc858ba6786e776601df4d131fcd0dee4bd
mak017 <makovskyy0983@gmail.com>
2021-02-18 22:16:12 +0200

    Try verify request with credentials: include

--- src/js/api/auth.js
+++ src/js/api/auth.js
@@ -21,11 +21,13 @@ export const loginRequest = async (email, password) => {
 
 export const verifyAuthRequest = async (token) => {
   const bearer = `Bearer ${token}`;
+  const headers = new Headers();
+  headers.append("Authorization", bearer);
   const response = await fetch(VERIFY(), {
     method: "POST",
-    // withCredentials: true,
-    credentials: "same-origin",
-    headers: new Headers({ Authorization: bearer }),
+    withCredentials: true,
+    credentials: "include",
+    headers,
   });
   const result = await response.json();
   return result;
