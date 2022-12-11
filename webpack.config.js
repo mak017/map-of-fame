b44f49741cc425ea9a8b0e312e6a9502892e9c40
mak017 <makovskyy0983@gmail.com>
2022-12-11 11:14:03 +0200

    MOf-191 - public path fix

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
     path: path.join(__dirname, "/public"),
     filename: "[name].js",
     chunkFilename: "[name].[id].js",
+    publicPath: "",
   },
   module: {
     rules: [
