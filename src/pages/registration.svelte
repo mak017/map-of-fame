f1433b906664458434d608491d23795c77faa5c4
mak017 <makovskyy0983@gmail.com>
2023-04-18 23:02:51 +0300

    MOF-259 - done

--- src/pages/registration.svelte
+++ src/pages/registration.svelte
@@ -7,8 +7,15 @@ import Modal from "../js/components/Modal.svelte";
 import RegistrationForm from "../js/components/auth/RegistrationForm.svelte";
 
 let inviteData = getInviteData();
+let isInviteError = false;
 </script>
 
-<Modal id="registration-modal" on:close={() => $goto("/")} title="Registration">
-  <RegistrationForm {inviteData} />
+<Modal
+  id="registration-modal"
+  on:close={() => $goto("/")}
+  title={isInviteError ? "Registr💩tion" : "Registration"}>
+  <RegistrationForm
+    {inviteData}
+    {isInviteError}
+    setInviteError={() => (isInviteError = true)} />
 </Modal>
